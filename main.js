7372690fb8344492bd6421c89bd906381f21c8ad
amirakhaled2027 <155392660+amirakhaled2027@users.noreply.github.com>
2024-01-05 04:02:04 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,65 @@
+//Type Conversion = change the datatype of a value to another
+// (strings, numbers, booleans)
+
+// let age = window.prompt("How old are you?");
+// age = Number(age);
+// age += 1;
+// console.log("Happy birthday! You are " + age + " years old!");
+// console.log(age, typeof age);
+
+
+//comment the previous code to make the following code work
+let x = "js";
+let y ="js";
+let z = "js";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+
+
+let a = "1";
+let b = "1";
+let c = "1";
+
+a = Number(a);
+b = String(b);
+c = Boolean(c);
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(c, typeof c);
+
+
+
+let m = "";
+let n = "";
+let o = "";
+
+m = Number(m);
+n = String(n);
+o = Boolean(o);
+
+console.log(m, typeof m);
+console.log(n, typeof n);
+console.log(o, typeof o);
+
+
+
+let q;
+let r;
+let s;
+
+q = Number(q);
+r = String(r);
+s = Boolean(s);
+
+console.log(q, typeof q);
+console.log(r, typeof r);
+console.log(s, typeof s);
+
